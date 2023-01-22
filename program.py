b94549975ee9bda1eae7c615352924e7f4584d12
mateo254 <mateuszprzybylowski@live.com>
2023-01-22 14:55:01 +0100

    Dodanie while.2

--- program.py
+++ program.py
@@ -1,10 +1,8 @@
 wiek = input("Podaj wiek użytkownika jako liczbę całkowitą: ")
 
-
 plec = input("Prosze podaj swoja plec: K-kobieta albo M-mężczyzna ")
 while plec.upper() != "K" and plec.upper() != "M":
     plec = input("Niepoprawna wartość. Prosze wprowadź M lub K: ")
-
 
 if wiek.isdigit() == False:
     exit("Wiek musi być liczbą albo nie jest liczbą całkowitą ")
@@ -16,3 +14,5 @@ elif wiek > 40:
     print("Uważaj w Twoim wieku na alkohol.")
 else:
     print("Jesteś za młody/a na alkohol.")
+
+
