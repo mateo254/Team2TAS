877b578cf6664857a102fbf3e3afef9456bc0e4e
Karolina259 <karolina.g@op.pl>
2023-01-22 14:42:21 +0100

    Obsluga zapytania plci

--- program.py
+++ program.py
@@ -9,3 +9,13 @@ elif wiek > 40:
     print("Uważaj w Twoim wieku na alkohol.")
 else:
     print("Jesteś za młody/a na alkohol.")
+
+# Karolina
+plec = input("Podaj plec: (K/M): ")
+if plec == "K" or plec == "k":
+	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+	print("Darmowy aperol dla Ciebie kochana")
+elif plec == "M" or plec == "m":
+	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+else:
+	print("Bledna odpowiedz. Wybierz K lub M zgodnie z poleceniem.")
