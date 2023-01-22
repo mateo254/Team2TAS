f1ea35d95e3e00e0b7fae0d9d0746ae3ce6fcf2e
glodomorek00 <cygmartyn@gmail.com>
2023-01-22 14:17:41 +0100

    Pierwsza wersja aplikacji dla dorosłych

--- program.py
+++ program.py
@@ -1,11 +1,36 @@
-wiek = input("Podaj wiek użytkownika jako liczbę całkowitą: ")
+
+region = input("Podaj swój region USA/EUR: ")
+plec = input("Podaj swoją płeć K/M: ")
+wiek = input("Podaj wiek użytkownika: ")
+
 if wiek.isdigit() == False:
-    exit("Wiek musi być liczbą albo nie jest liczbą całkowitą ")
+    exit("Wiek musi być liczbą całkowitą.")
+
 wiek = int(wiek)
-if wiek >= 18 and wiek<=40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
-elif wiek > 40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
-    print("Uważaj w Twoim wieku na alkohol.")
-else:
-    print("Jesteś za młody/a na alkohol.")
+region = str(region)
+plec = str(plec)
+
+if region == "USA":
+    print("Twój region to USA")
+    if wiek >= 21 and wiek < 30:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+    elif wiek >= 30 and plec == "K":
+        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów. Mamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
+    elif wiek >= 30 and plec == "M":
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+    else:
+        exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+
+if region == "EUR":
+    print("Twój region to EUR")
+    if wiek >= 18 and wiek < 30:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+    elif wiek >= 30 and plec == "K":
+        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów. Mamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
+    elif wiek >= 30 and plec == "M":
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+    else:
+        exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+
+
+
